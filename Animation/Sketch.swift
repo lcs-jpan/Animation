fbf92f10f8613154e9b53dfb424cac0240035d41
Pan Jason <jpan@lcs-mba3130.lcs.on.ca>
2017-11-03 10:49:24 -0400

    circle move to the right

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var dx : Int
     
     // This function runs once
     override init() {
@@ -18,16 +19,39 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
+        dx = 2
+        
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
         // Change position
-        x += 1
+        x += dx
+        
+        dx = 2
         
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        
+        
+        // Clean up - draw a white rectangle over the entire canvascanvas.fillColour = colour.white
+        canvas.fillColor = Color.white
+        canvas.drawRoundedRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
+        // Chnage position
+        x += dx
+        
+        // Make the circle bounce on the right edge
+        if x > 500 { // start of block}
+            dx = -2 // move left
+        } // end of block
+        
+        // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        
+
         
     }
     
